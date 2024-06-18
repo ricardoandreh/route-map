b4bbe268e270f17545cb70e6d0d565b12284aa81
Ricardo André <ricardo.andre.ifc@gmail.com>
2024-06-18 00:26:15 -0300

    feat: config Django Rest Framework and add tests

---
+++ trip/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+
+from trip.models import Route
+
+
+class RouteSerializer(ModelSerializer):
+  class Meta:
+    model = Route
+    fields = "__all__"
--- trip/tests.py
+++ trip/tests.py
@@ -1,3 +1,69 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from trip.models import Route
+from trip.serializers import RouteSerializer
+
+
+class RouteTests(APITestCase):
+    def setUp(self):
+        self.url = reverse("route-list")
+        self.user = User.objects.create(
+            username="rick",
+            email="rick@email.com",
+            password=make_password("strongpass123")
+        )
+
+    def test_list_empty_route(self):
+        """
+        Ensure we can list a empty route object.
+        """
+        response = self.client.get(self.url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 0)
+
+    def test_list_all_routes(self):
+        """
+        Ensure we can list all route objects.
+        """
+        Route.objects.create(user=self.user, trip='{"test", 1}')
+        Route.objects.create(user=self.user, trip='{"test", 2}')
+        Route.objects.create(user=self.user, trip='{"test", 3}')
+        Route.objects.create(user=self.user, trip='{"test", 4}')
+        response = self.client.get(self.url, format="json")
+        routes = Route.objects.all()
+        serializer = RouteSerializer(routes, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 4)
+        self.assertEqual(response.data, serializer.data)
+
+
+    def test_create_route(self):
+        """
+        Ensure we can create a route object.
+        """
+        data = {
+            "user": self.user.id,
+            "trip": {
+                "origin": {
+                    "address" : "6633+Hollywood+Boulevard",
+                },
+                "destination": {
+                    "address" : "234+Vine+Street",
+                },
+                "intermediates": [
+                    {"address": "204+Marshall+Road"},
+                    {"address": "31+West+Street"},
+                    {"address": "776+Miracle+Mile"},
+                ],
+                "travelMode": "DRIVE",
+                "routingPreference": "TRAFFIC_AWARE",
+            },
+        }
+        response = self.client.post(self.url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Route.objects.get().trip["origin"]["address"], "6633+Hollywood+Boulevard")
