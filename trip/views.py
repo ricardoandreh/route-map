56cfab723031ace6c68c151a344608f498f75e34
Ricardo André <ricardo.andre.ifc@gmail.com>
2024-07-14 23:26:02 -0300

    test: basic JSONField schema validation in Views

--- trip/views.py
+++ trip/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
+
+from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 
 from trip.models import Route
@@ -7,3 +10,12 @@ from trip.serializers import RouteSerializer
 class RouteViewSet(ModelViewSet):
   queryset = Route.objects.all()
   serializer_class = RouteSerializer
+
+  def create(self, request, *args, **kwargs):
+    if "origin" not in request.data["trip"] or "address" not in request.data["trip"]["origin"]:
+      return HttpResponse('"address" in "origin" field in Trip is required', status=status.HTTP_400_BAD_REQUEST)
+    
+    print("testing")
+
+    return super().create(request, *args, **kwargs)
+  
